1ebb7339993aed8d5d91864c7612a54949ae8f7f
General_Vetrovka <mathieu.devries@bluewin.ch>
2024-10-18 10:31:12 +0200

    Update 20.9.2024.cs

--- 20.9.2024.cs
+++ 20.9.2024.cs
@@ -25,6 +25,7 @@
                 {
 
                 }
+                
                 else
                 {
                     Console.WriteLine("Strings are Unnececary!");
@@ -37,9 +38,11 @@
                     Console.WriteLine("Now enter a valid Number: ");
                     continue;
                 }
+                
                 if (Number > 0)
                 {
                 }
+                
                 else
                 {
                     Console.WriteLine("Your Number is Deeper than the set Range!");
@@ -52,9 +55,11 @@
                     Console.WriteLine("Enter a Number between 1 and 100: ");
                     continue;
                 }
+                
                 if (Number < 101)
                 {
                 }
+                
                 else
                 {
                     Console.WriteLine("Your Number is Higher than the set Range!");
@@ -74,12 +79,14 @@
                     Console.WriteLine("The secret Number is smaller!");
                     versuche++;
                 }
+                
                 else if (Number < RandomNumber)
                 {
                     repeat = true;
                     Console.WriteLine("The Secret Number is bigger!");
                     versuche++;
                 }
+                
                 else if (Number == RandomNumber)
                 {
                     Console.WriteLine("Right Number");
@@ -91,23 +98,29 @@
                 {
                     Random random = new Random();
                     int randomly = random.Next(1, 3);
+                    
                     if (randomly == 1)
                     {
+                        
                         if (enteredshit > 0)
                         {
                             Console.WriteLine("You are shit and entered " + enteredshit + " times a not valid variable! YOU ARE AN IDIOT!");
                             continue;
                         }
                     }
+                    
                     if (randomly == 2)
                     {
+                        
                         if (enteredshit > 0)
                         {
                             Console.WriteLine("You 3 year old Idiot you entered  " + enteredshit + " times a not valid variable! Why you are so dumb?");
                             continue;
                         }
+                        
                         if (randomly == 3)
                         {
+                            
                             if (enteredshit > 0)
                             {
                                 Console.WriteLine("This Idiot entered" + enteredshit + " times a wrong variable!");
@@ -125,9 +138,7 @@
                     Thread.Sleep(500);
                     Console.WriteLine("Enter a new Number");
                 }
-
             }
-
         }
     }
 }
